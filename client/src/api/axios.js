6c3bff8aefd361e5bf2b867d0eb9c087b225f285
mirathecoder <mirasinghbhoi@gmail.com>
2026-05-22 10:28:11 +0530

    docs: add explanatory comments for Axios setup and request interceptor

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+// Create an Axios instance with base URL and cookie support enabled
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
+
+// Interceptor to inject JWT token from localStorage into Authorization headers if present
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token"); 
   if (token) {
